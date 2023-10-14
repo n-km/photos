f051b039f862826badc8a34c43c0edd58d5ea941
n-km <de.nilskoltermann@gmail.com>
2023-10-14 20:58:32 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Photos</title>
+    <link rel="stylesheet" href="main.css">
+    <style>
+        body {
+            font-family: arial;
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+   <?php 
+   echo "Hallo, lalalla";
+    
+   ?>
+</body>
+</html>
